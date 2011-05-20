432c1353aea350641dcec423b33ead8a9471b645
Tommaso Passi <masolino@gmail.com>
2011-05-20 14:47:52 +0200

    Modified order and size of flickr images

--- js/common.js
+++ js/common.js
@@ -5,27 +5,31 @@ jQuery(function(){
     user_id: '69119358@N00',
     thumb_size: 's',
     size: 'b',
-    per_page: 12
+    per_page: 12,
+    sort: "date-posted-desc"
   });
   jQuery(".people").flickr({
     api_key: "bbf6a46e7e65a5022da9cae9355ed1a3",
     type: 'photoset',
     photoset_id: '72157613427796892',
     thumb_size: 's',
-    size: 'b'
+    per_page: 12,
+    sort: "date-posted-desc"
   });
   jQuery(".places").flickr({
     api_key: "bbf6a46e7e65a5022da9cae9355ed1a3",
     type: 'photoset',
     photoset_id: '72157617650746490',
     thumb_size: 's',
-    size: 'b'
+    per_page: 12,
+    sort: "date-posted-desc"
   });
   jQuery(".things").flickr({
     api_key: "bbf6a46e7e65a5022da9cae9355ed1a3",
     type: 'photoset',
     photoset_id: '72157616886156036',
     thumb_size: 's',
-    size: 'b'
+    per_page: 12,
+    sort: "date-posted-desc"
   });
 });
